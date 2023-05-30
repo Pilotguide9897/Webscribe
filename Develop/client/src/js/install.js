6a72e4027bf6e7427a4801058be2c8cde1c9e755
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-05-30 20:09:18 -0300

    removed unwanted comments

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,6 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// window.addEventListener('beforeinstallprompt', (event) => {
-//   event.preventDefault();
-// });
-
-// Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
 
@@ -15,14 +10,6 @@ window.addEventListener("beforeinstallprompt", (event) => {
     butInstall.textContent = "Installed";
   });
 });
-
-// Click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async () => {
-//   butInstall.setAttribute('disabled', true);
-//   butInstall.textContent = 'Installed';
-// });
-
-// Click event handler on the `butInstall` element
 
 // Handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
